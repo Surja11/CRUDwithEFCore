2d1b6df0bba1d9dc601b70fb5a30b32318a82b11
Surja11 <stha.surja123@gmail.com>
2025-05-04 19:05:31 +0545

    Add project files.

---
+++ StudentPortal/Controllers/StudentsController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Data;
+using StudentPortal.Models;
+using StudentPortal.Models.Entity;
+
+namespace StudentPortal.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddStudentViewModel viewModel)
+        {
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+            dbContext.Students.Add(student);
+            dbContext.SaveChanges();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            var students = dbContext.Students.ToList();
+            return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var student = dbContext.Students.Find(id);
+
+
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student viewModel)
+        {
+           var student = dbContext.Students.Find(viewModel.Id);
+            if(student != null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribed = viewModel.Subscribed;
+                dbContext.SaveChanges();
+            }
+
+            return RedirectToAction("List","Students");
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Student viewModel)
+        {
+            var student =dbContext.Students.AsNoTracking().FirstOrDefault(x => x.Id == viewModel.Id);
+            if(student is not null)
+            {
+                dbContext.Students.Remove(viewModel);
+                dbContext.SaveChanges();
+            }
+
+            return RedirectToAction("List","Students");
+        }
+
+        public IActionResult SubscribedList()
+        {
+            var slist =dbContext.Students.Where(x => x.Subscribed).ToList();
+            return View(slist);
+        }
+
+        public IActionResult UnSubscribe()
+        {
+            var slist = dbContext.Students.Where(x => x.Name.StartsWith("S") && x.Subscribed).ToList();
+            foreach(var item in slist)
+            {
+                item.Subscribed = false;   
+            }
+            dbContext.SaveChanges();
+
+            //var list = dbContext.Students.Where(x => x.Subscribed).ToList();
+            //return View(list);
+            return RedirectToAction("SubscribedList","Students");
+        }
+    }
+}
---
+++ StudentPortal/Models/Entity/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace StudentPortal.Models.Entity
+{
+    public class Student
+    {
+        public Guid Id { get; set; }    
+        
+        public string Name { get; set; }
+        
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool Subscribed {  get; set; }   
+
+    }
+}
